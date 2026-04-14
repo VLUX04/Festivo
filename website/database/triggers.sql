60011f22853c3e0077416742e42305ae586a9442
Bernardo Costa <up202207579@up.pt>
2026-04-14 18:00:59 +0100

    Added trigger that prevents self-invites on professional_invite

---
+++ website/database/triggers.sql
@@ -0,0 +1,16 @@
+-- -------------------------------------------------------------
+-- 1. Prevent self-invites on professional_invite
+-- -------------------------------------------------------------
+CREATE OR REPLACE FUNCTION fn_prevent_self_invite()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.sender_id = NEW.receiver_id THEN
+        RAISE EXCEPTION 'A professional cannot invite themselves (sender_id = receiver_id = %).', NEW.sender_id;
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+ 
+CREATE TRIGGER trg_prevent_self_invite
+BEFORE INSERT ON professional_invite
+FOR EACH ROW EXECUTE FUNCTION fn_prevent_self_invite();
