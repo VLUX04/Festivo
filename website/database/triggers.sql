6702c295921ca5e4bd841d342bab6b21fd1bb675
Bernardo Costa <up202207579@up.pt>
2026-04-14 18:09:15 +0100

    Added trigger that enforces event date order.

--- website/database/triggers.sql
+++ website/database/triggers.sql
@@ -1,5 +1,5 @@
 -- -------------------------------------------------------------
--- 1. Prevent self-invites on professional_invite
+-- Prevent self-invites on professional_invite
 -- -------------------------------------------------------------
 CREATE OR REPLACE FUNCTION fn_prevent_self_invite()
 RETURNS TRIGGER AS $$
@@ -14,3 +14,20 @@ $$ LANGUAGE plpgsql;
 CREATE TRIGGER trg_prevent_self_invite
 BEFORE INSERT ON professional_invite
 FOR EACH ROW EXECUTE FUNCTION fn_prevent_self_invite();
+
+-- -------------------------------------------------------------
+-- Enforce event date order (edate >= sdate)
+-- -------------------------------------------------------------
+CREATE OR REPLACE FUNCTION fn_enforce_event_date_order()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.edate < NEW.sdate THEN
+        RAISE EXCEPTION 'Event end date (%) cannot be before start date (%).', NEW.edate, NEW.sdate;
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql
+
+CREATE TRIGGER trg_enforce_event_date_order
+BEFORE INSERT OR UPDATE ON events
+FOR EACH ROW EXECUTE FUNCTION fn_enforce_event_date_order()
