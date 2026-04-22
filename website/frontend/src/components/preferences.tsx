d30e7dbb89e179d60db7889cd144c59f8161bb40
Bernardo Costa <up202207579@up.pt>
2026-04-22 16:01:22 +0100

    All 3 user types have same preference choices.

--- website/frontend/src/components/preferences.tsx
+++ website/frontend/src/components/preferences.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import profile_icon from "../icons/profile.png";
+import location_icon from "../icons/map.png";
 
 type AccountTypeBadgeProps = {
     accountType: string;
@@ -10,25 +11,48 @@ const Preferences: React.FC<AccountTypeBadgeProps> = ({ accountType }) => {
     const navigate = useNavigate();
 
     return (
-        <div className="self-center w-[45em] h-auto px-6 py-5 m-7 bg-[#1a0f10] border-3 border-[#a89a60] flex flex-row items-center justify-between shadow-[15px_15px_0_0_#231c16]">
-            <div className="flex flex-row items-center gap-4">
-                <div className="w-14 h-14 border-3 border-[#a89a60] flex items-center justify-center">
-                    <img src={profile_icon} alt="Profile" className="w-8 h-8 object-contain"/>
+       <div className="flex flex-col items-center w-full">
+            <div className="w-[45em] h-auto px-6 py-5 m-7 bg-[#1a0f10] border-3 border-[#fff3b0] flex flex-row items-center justify-between shadow-[15px_15px_0_0_#231c16]">
+                <div className="flex flex-row items-center gap-4">
+                    <div className="w-14 h-14 border-3 border-[#a89a60] flex items-center justify-center">
+                        <img src={profile_icon} alt="Profile" className="w-8 h-8 object-contain"/>
+                    </div>
+                    <div className="text-left">
+                        <p className="text-[#a89a60] text-sm">Account Type</p>
+                        <h2 className="text-[#fff3b0] text-xl font-bold">{accountType}</h2>
+                    </div>
                 </div>
-                <div className="text-left">
-                    <p className="text-[#a89a60] text-sm">Account Type</p>
-                    <h2 className="text-[#fff3b0] text-xl font-bold">{accountType}</h2>
-                </div>
+                <button
+                    onClick={() => navigate("/account")}
+                    className="group transition duration-250 ease-in-out px-6 py-2 bg-[#1a0f10] hover:bg-[#fff3b0] border-3 border-[#483d30] hover:border-[#fff3b0] flex items-center justify-center cursor-pointer"
+                >
+                    <span className="text-[#fff3b0] group-hover:text-[#540b0e]">
+                        Change
+                    </span>
+                </button>
             </div>
-            <button
-                onClick={() => navigate("/account")}
-                className="group transition duration-250 ease-in-out px-6 py-2 bg-[#1a0f10] hover:bg-[#fff3b0] border-3 border-[#483d30] hover:border-[#fff3b0] flex items-center justify-center cursor-pointer"
-            >
-                <span className="text-[#fff3b0] group-hover:text-[#540b0e]">
-                    Change
-                </span>
-            </button>
-        </div>
+
+            <div className="w-[45em] h-auto px-6 py-5 m-7 bg-[#1a0f10] border-3 border-[#483d30] flex flex-col shadow-[15px_15px_0_0_#231c16]">
+                <div className="text-left">
+                    <h2 className="text-[#fff3b0] text-3xl font-bold mb-3">About you</h2>
+                    <p className="text-[#fff3b0]">Tell us about yourself</p>
+                </div>
+                <textarea
+                    placeholder="Your story, interests, or what you do..."
+                    className="w-full mt-4 mb-2 p-3 bg-[#0a0505] border-3 border-[#483d30] text-[#fff3b0] placeholder-[] resize-none h-32 focus:outline-none focus:border-[#a89a60]"/>
+            </div>
+
+            <div className="w-[45em] h-auto px-6 py-5 m-7 bg-[#1a0f10] border-3 border-[#483d30] flex flex-col shadow-[15px_15px_0_0_#231c16]">
+                <div className="flex flex-row items-center gap-2 mb-3">
+                    <img src={location_icon} alt="Location" className="w-8 h-8 object-contain"/>
+                    <h2 className="text-[#fff3b0] text-3xl font-bold">Location</h2>
+                </div>
+                <p className="text-[#fff3b0]">Where you are based</p>
+                <textarea
+                    placeholder="e.g., Lisboa, Porto, Coimbra, ..."
+                    className="w-full mt-4 mb-2 p-3 bg-[#0a0505] border-3 border-[#483d30] text-[#fff3b0] placeholder-[] resize-none h-14 focus:outline-none focus:border-[#a89a60]"/>
+            </div>
+        </div> 
     );
 };
 
